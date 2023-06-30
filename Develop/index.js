f8b4e3bf2fcbc5d08974ef3476bc8ae930f8e852
Lilac <jonasjosh7@gmail.com>
2023-06-30 15:49:44 -0600

    basic svg maker, needs fixed

--- Develop/index.js
+++ Develop/index.js
@@ -1,11 +1,11 @@
-const { Builder, Path } = require('svg-builder');
-const inquirer = require('inquirer');
-const fs = require('fs');
+const { DOMParser, XMLSerializer } = require('xmldom');
 
 // Function to generate the SVG
 function generateLogo(text, shape, textColor, backgroundColor) {
-    const builder = new Builder();
-
+  const parser = new DOMParser();
+  const serializer = new XMLSerializer();
+  const dom = parser.parseFromString('<svg xmlns="http://www.w3.org/2000/svg"></svg>', 'application/xml');
+  const svgElement = dom.documentElement;
       // Handle text color input
   if (textColor.startsWith('#')) {
     textColor = textColor.substring(1); // Remove the '#' character
@@ -16,57 +16,82 @@ function generateLogo(text, shape, textColor, backgroundColor) {
     backgroundColor = backgroundColor.substring(1); // Remove the '#' character
   }
     
+// Create background style element
+const styleElement = dom.createElement('style');
+styleElement.textContent = `svg { background-color: #${backgroundColor}; width: 200px; height: 200px; }`;
+
+// Append style element to SVG
+svgElement.appendChild(styleElement);
+
 
     // Define shape properties
-    let shapePath;
+    let shapeElement;
     let shapeClass;
     switch (shape) {
-        case 'Circle':
-            shapePath = builder.circle().cx(100).cy(100).r(50);
-            shapeclass = 'circle';
-            break;
-        case 'Square':
-            shapepath = builder.rect().x(50).y(50).width(100).height(100);
-            shapeclass = 'square';
-            break;
-        case 'Triangle':
-            shapePath = builder.path()
-            .moveTo(100, 50)
-            .lineTo(50, 150)
-            .lineTo(150, 150)
-            .close();
-          shapeClass = 'triangle';
+      case 'Circle':
+        shapeElement = dom.createElement('circle');
+        shapeElement.setAttribute('cx', '100');
+        shapeElement.setAttribute('cy', '100');
+        shapeElement.setAttribute('r', '50');
+        shapeClass = 'circle';
+        break;
+      case 'Square':
+        shapeElement = dom.createElement('rect');
+        shapeElement.setAttribute('x', '50');
+        shapeElement.setAttribute('y', '50');
+        shapeElement.setAttribute('width', '100');
+        shapeElement.setAttribute('height', '100');
+        shapeClass = 'square';
+        break;
+      case 'Triangle':
+        shapeElement = dom.createElement('path');
+        shapeElement.setAttribute('d', 'M100 50 L50 150 L150 150 Z');
+        shapeClass = 'triangle';
           break;
         default:
             console.log('Invalid shape selected.');
             return;
     }
 
-    // Building the svg
-    const svg = builder
-    .svg()
-    .width(200)
-    .height(200)
-    .attr('xmlns', 'http://www.w3.org/2000/svg')
-    .attr('viewBox', '0 0 200 200')
-    .style(`.${shapeClass}`, `fill: none; stroke-width: 3; stroke-linecap: round; stroke-linejoin: round;`)
-    .style(`.${shapeClass === 'square' || shapeClass === 'triangle' ? shapeClass : ''}`, `stroke-dasharray: 10;`)
-    .style('text', `fill: ${textColor}; font-size: 48px; text-anchor: middle; dominant-baseline: middle;`)
-    .attr('class', shapeClass)
-    .add(shapePath)
-    .text()
-    .x(100)
-    .y(100)
-    .tspan()
-    .text(text);
+  // Set shape element attributes
+  shapeElement.setAttribute('class', shapeClass);
+  shapeElement.setAttribute('fill', 'none');
+  shapeElement.setAttribute('stroke-width', '3');
+  shapeElement.setAttribute('stroke-linecap', 'round');
+  shapeElement.setAttribute('stroke-linejoin', 'round');
 
-  return svg.build();
+  // Set additional style for square and triangle shapes
+  if (shapeClass === 'square' || shapeClass === 'triangle') {
+    shapeElement.setAttribute('stroke-dasharray', '10');
+  }
+
+  // Create text element
+  const textElement = dom.createElement('text');
+  textElement.setAttribute('x', '100');
+  textElement.setAttribute('y', '100');
+  textElement.setAttribute('fill', textColor);
+  textElement.setAttribute('font-size', '48px');
+  textElement.setAttribute('text-anchor', 'middle');
+  textElement.setAttribute('dominant-baseline', 'middle');
+  const tspanElement = dom.createElement('tspan');
+  tspanElement.textContent = text;
+  textElement.appendChild(tspanElement);
+
+  // Append elements to SVG
+  svgElement.appendChild(shapeElement);
+  svgElement.appendChild(textElement);
+
+  const svgMarkup = serializer.serializeToString(dom);
+
+  return svgMarkup;
 }
 
 // Prompts for user input
-inquirer
-    .createPromptModule([
-        {
+async function runLogoMaker(){
+  const {default: inquirer} = await import('inquirer');
+  const fs = require('fs');
+  const answers = await inquirer.prompt([
+    {
             type: 'input',
             name: 'text',
             message: 'Enter the text for the logo:'
@@ -81,7 +106,7 @@ inquirer
             type: 'input',
             name: 'textColor',
             message: 'Enter the text color:',
-            when: answers => answers.textColorChoice === 'HexFormat'
+            when: answers => answers.textColorChoice === 'Hex Format'
         },
         {
             type: 'input',
@@ -113,18 +138,20 @@ inquirer
             message: 'Choose a shape:',
             choices: ['Circle', 'Square', 'Triangle']
           }
-    ])
-    .then(answers => {
-       // Generate logo
-        const logo = generateLogo(
-            answers.text,
-            answers.shape,
-            answers.textColor,
-            answers.backgroundColor
-        );
+
+        ]);
+      const logo = generateLogo(
+        answers.text,
+        answers.shape,
+        answers.textColor,
+        answers.backgroundColor
+    );
 
         // Save the logo as an SVG file
         fs.writeFileSync('logo.svg', logo);
         console.log('Logo generated and saved successfully!');
+       }
 
-    });
+       runLogoMaker().catch(error => {
+        console.error('Error:', error);
+       });
