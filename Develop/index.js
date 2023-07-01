6a31c16c598acf6c861aea9c46bd9f18eca02ce0
Lilac <jonasjosh7@gmail.com>
2023-07-01 02:51:30 -0600

    updates to logo maker

--- Develop/index.js
+++ Develop/index.js
@@ -1,27 +1,67 @@
 const { DOMParser, XMLSerializer } = require('xmldom');
 
+// Function to check if a string is a valid color name
+function isValidColorName(colorName) {
+  const colorNames = [
+    'aliceblue', 'antiquewhite', 'aqua', 'aquamarine', 'azure', 'beige', 'bisque', 'black',
+    'blanchedalmond', 'blue', 'blueviolet', 'brown', 'burlywood', 'cadetblue', 'chartreuse',
+    'chocolate', 'coral', 'cornflowerblue', 'cornsilk', 'crimson', 'cyan', 'darkblue', 'darkcyan',
+    'darkgoldenrod', 'darkgray', 'darkgreen', 'darkkhaki', 'darkmagenta', 'darkolivegreen',
+    'darkorange', 'darkorchid', 'darkred', 'darksalmon', 'darkseagreen', 'darkslateblue',
+    'darkslategray', 'darkturquoise', 'darkviolet', 'deeppink', 'deepskyblue', 'dimgray',
+    'dodgerblue', 'firebrick', 'floralwhite', 'forestgreen', 'fuchsia', 'gainsboro', 'ghostwhite',
+    'gold', 'goldenrod', 'gray', 'green', 'greenyellow', 'honeydew', 'hotpink', 'indianred', 'indigo',
+    'ivory', 'khaki', 'lavender', 'lavenderblush', 'lawngreen', 'lemonchiffon', 'lightblue',
+    'lightcoral', 'lightcyan', 'lightgoldenrodyellow', 'lightgray', 'lightgreen', 'lightpink',
+    'lightsalmon', 'lightseagreen', 'lightskyblue', 'lightslategray', 'lightsteelblue',
+    'lightyellow', 'lime', 'limegreen', 'linen', 'magenta', 'maroon', 'mediumaquamarine',
+    'mediumblue', 'mediumorchid', 'mediumpurple', 'mediumseagreen', 'mediumslateblue',
+    'mediumspringgreen', 'mediumturquoise', 'mediumvioletred', 'midnightblue', 'mintcream',
+    'mistyrose', 'moccasin', 'navajowhite', 'navy', 'oldlace', 'olive', 'olivedrab', 'orange',
+    'orangered', 'orchid', 'palegoldenrod', 'palegreen', 'paleturquoise', 'palevioletred',
+    'papayawhip', 'peachpuff', 'peru', 'pink', 'plum', 'powderblue', 'purple', 'rebeccapurple',
+    'red', 'rosybrown', 'royalblue', 'saddlebrown', 'salmon', 'sandybrown', 'seagreen',
+    'seashell', 'sienna', 'silver', 'skyblue', 'slateblue', 'slategray', 'snow', 'springgreen',
+    'steelblue', 'tan', 'teal', 'thistle', 'tomato', 'turquoise', 'violet', 'wheat', 'white',
+    'whitesmoke', 'yellow', 'yellowgreen'
+  ];
+  return colorNames.includes(colorName.toLowerCase());
+}
+
 // Function to generate the SVG
-function generateLogo(text, shape, textColor, backgroundColor) {
+function generateLogo(text, shape, textColor, backgroundColor, answers) {
   const parser = new DOMParser();
   const serializer = new XMLSerializer();
   const dom = parser.parseFromString('<svg xmlns="http://www.w3.org/2000/svg"></svg>', 'application/xml');
   const svgElement = dom.documentElement;
-      // Handle text color input
-  if (textColor.startsWith('#')) {
-    textColor = textColor.substring(1); // Remove the '#' character
-  }
 
-  // Handle background color input
-  if (backgroundColor.startsWith('#')) {
-    backgroundColor = backgroundColor.substring(1); // Remove the '#' character
-  }
+
+
+    // Handle text color input
+    if (textColor.startsWith('#')) {
+      textColor = textColor.substring(1); // Remove the '#' character
+    } else if (!isValidColorName(textColor)) {
+      console.log('Invalid text color specified.');
+      return;
+    }
     
-// Create background style element
-const styleElement = dom.createElement('style');
-styleElement.textContent = `svg { background-color: #${backgroundColor}; width: 200px; height: 200px; }`;
+// Handle background color input
+if (answers.backgroundColorChoice === 'Hex Format') {
+  if (!answers.backgroundColor.match(/^#[0-9A-Fa-f]{6}$/)) {
+    console.log('Invalid background color specified.');
+    return;
+  }
+} else if (answers.backgroundColorChoice === 'Color Name') {
+  if (!isValidColorName(answers.backgroundColor)) {
+    console.log('Invalid background color specified.');
+    return;
+  }
+}
 
-// Append style element to SVG
-svgElement.appendChild(styleElement);
+
+    
+ // Set background color
+ svgElement.setAttribute('style', `background-color: ${answers.backgroundColor}`);
 
 
     // Define shape properties
@@ -60,6 +100,11 @@ svgElement.appendChild(styleElement);
   shapeElement.setAttribute('stroke-linecap', 'round');
   shapeElement.setAttribute('stroke-linejoin', 'round');
 
+
+
+
+
+
   // Set additional style for square and triangle shapes
   if (shapeClass === 'square' || shapeClass === 'triangle') {
     shapeElement.setAttribute('stroke-dasharray', '10');
@@ -77,9 +122,12 @@ svgElement.appendChild(styleElement);
   tspanElement.textContent = text;
   textElement.appendChild(tspanElement);
 
-  // Append elements to SVG
-  svgElement.appendChild(shapeElement);
-  svgElement.appendChild(textElement);
+// Append text element to SVG first
+svgElement.appendChild(textElement);
+
+// Append shape element to SVG after the text
+svgElement.appendChild(shapeElement);
+
 
   const svgMarkup = serializer.serializeToString(dom);
 
@@ -140,12 +188,15 @@ async function runLogoMaker(){
           }
 
         ]);
-      const logo = generateLogo(
-        answers.text,
-        answers.shape,
-        answers.textColor,
-        answers.backgroundColor
-    );
+
+        
+  const logo = generateLogo(
+    answers.text,
+    answers.shape,
+    answers.textColor,
+    answers.backgroundColor,
+    answers
+);
 
         // Save the logo as an SVG file
         fs.writeFileSync('logo.svg', logo);
