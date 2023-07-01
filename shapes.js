befb282de1bd96b868a142a3baeffbe112e2178d
Lilac <jonasjosh7@gmail.com>
2023-07-01 16:22:25 -0600

    jest tests running properly

--- shapes.js
+++ shapes.js
@@ -1,33 +1,49 @@
-
-// Define the classes for shapes
-    class Circle {
-    constructor(radius) {
-      this.radius = radius;
-    }
-  
-    render() {
-      return `<circle cx="100" cy="100" r="${this.radius}" />`;
-    }
-  }
-  
-    class Square {
-    constructor(sideLength) {
-      this.sideLength = sideLength;
-    }
-  
-    render() {
-      return `<rect x="50" y="50" width="${this.sideLength}" height="${this.sideLength}" />`;
-    }
-  }
-  
-    class Triangle {
-    render() {
-      return '<path d="M100 50 L50 150 L150 150 Z" />';
-    }
+class Circle {
+  constructor(radius) {
+    this.radius = radius;
+    this.color = 'black'; // Default color
   }
 
-  module.exports = {
-    Circle,
-    Square,
-    Triangle,
-  };
+  setColor(color) {
+    this.color = color;
+  }
+
+  render() {
+    return `<circle cx="100" cy="100" r="${this.radius}" fill="${this.color}" />`;
+  }
+}
+
+class Square {
+  constructor(sideLength) {
+    this.sideLength = sideLength;
+    this.color = 'black'; // Default color
+  }
+
+  setColor(color) {
+    this.color = color;
+  }
+
+  render() {
+    return `<rect x="50" y="50" width="${this.sideLength}" height="${this.sideLength}" fill="${this.color}" />`;
+  }
+}
+
+class Triangle {
+  constructor() {
+    this.color = 'black'; // Default color
+  }
+
+  setColor(color) {
+    this.color = color;
+  }
+
+  render() {
+    return `<path d="M100 50 L50 150 L150 150 Z" fill="${this.color}" />`;
+  }
+}
+
+module.exports = {
+  Circle,
+  Square,
+  Triangle,
+};
--- shapes.test.js
+++ shapes.test.js
@@ -1,6 +1,19 @@
-const {Circle, Square, Triangle} = require('./index'); 
+const { Circle, Square, Triangle } = require('./shapes');
 
-const shape = new Triangle();
-shape.setColor("blue");
-expect(shape.render()).toEqual('<polygon points="150, 18 244, 182 56, 182" fill="blue" />'); 
+test('Triangle shape renders correctly', () => {
+    const shape = new Triangle();
+    shape.setColor("blue");
+    expect(shape.render()).toEqual('<path d="M100 50 L50 150 L150 150 Z" fill="blue" />');
+  });
   
+test('Circle shape renders correctly', () => {
+    const shape = new Circle(50); // Create a circle with radius 50
+    shape.setColor("red"); // Set the color of the circle
+    expect(shape.render()).toEqual('<circle cx="100" cy="100" r="50" fill="red" />');
+  });
+  
+  test('Square shape renders correctly', () => {
+    const shape = new Square(80); // Create a square with side length 80
+    shape.setColor("green"); // Set the color of the square
+    expect(shape.render()).toEqual('<rect x="50" y="50" width="80" height="80" fill="green" />');
+  });
